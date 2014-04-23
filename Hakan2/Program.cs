4a64f9c1ccd55b4ec10c747840cc036cdec5670e
Håkan Pettersson <hakan.pettersson@expressen.se>
2014-04-23 11:12:27 +0200

    first commit

---
+++ Hakan2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hakan2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
